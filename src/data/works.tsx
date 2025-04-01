ba8ef1d6f52b7f5907645dc613d736ead63a40c1
Keita Ito <keitaito@keitaito.net>
2025-04-02 02:48:45 +0900

    feat: Worksに新しいプロジェクトを追加

--- src/data/works.tsx
+++ src/data/works.tsx
@@ -27,4 +27,30 @@ export const works = [
     url: "#",
     thumbnail: "/sample-cover.jpg",
   },
+  {
+    title: "第74回調布祭ウェブサイト",
+    description: (
+      <p>2024年11月に開催された電通大の学園祭「調布祭」のウェブサイト</p>
+    ),
+    url: "#",
+    thumbnail: "/sample-cover.jpg",
+  },
+  {
+    title: "UEC 3D Map",
+    description: <p>電気通信大学をウェブ上で3Dで見ることができます</p>,
+    url: "#",
+    thumbnail: "/sample-cover.jpg",
+  },
+  {
+    title: "りさプロ",
+    description: (
+      <p>
+        授業で作成したJavaのゲームです。
+        <br />
+        AWSを組み込んだオンライン機能やウェブサイトを作成しました
+      </p>
+    ),
+    url: "#",
+    thumbnail: "/sample-cover.jpg",
+  },
 ] satisfies Work[];
