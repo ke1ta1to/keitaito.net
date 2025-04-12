c6c48f4759652366dcfac52aef88ffb9fc0abaaa
Keita Ito <keitaito@keitaito.net>
2025-04-13 02:00:35 +0900

    feat: dotenvを追加し、環境変数を使用してAWSリソースの設定を行う

--- infra/lib/portfolio-app-stack.ts
+++ infra/lib/portfolio-app-stack.ts
@@ -1,14 +1,29 @@
 import * as cdk from "aws-cdk-lib";
+import * as certificatemanager from "aws-cdk-lib/aws-certificatemanager";
 import * as cloudfront from "aws-cdk-lib/aws-cloudfront";
 import * as cloudfrontOrigins from "aws-cdk-lib/aws-cloudfront-origins";
 import * as ecr from "aws-cdk-lib/aws-ecr";
 import * as lambda from "aws-cdk-lib/aws-lambda";
+import * as route53 from "aws-cdk-lib/aws-route53";
+import * as route53targets from "aws-cdk-lib/aws-route53-targets";
 import * as s3 from "aws-cdk-lib/aws-s3";
 import type { Construct } from "constructs";
 
+interface PortfolioAppStackProps extends cdk.StackProps {
+  customDomainName: string;
+  hostedZoneDomainName: string;
+  certificateArn: string;
+}
+
 export class PortfolioAppStack extends cdk.Stack {
-  constructor(scope: Construct, id: string, props?: cdk.StackProps) {
+  constructor(scope: Construct, id: string, props: PortfolioAppStackProps) {
     super(scope, id, props);
+
+    const certificate = certificatemanager.Certificate.fromCertificateArn(
+      this,
+      "PortfolioCertificate",
+      props.certificateArn,
+    );
 
     const lambdaFunction = new lambda.DockerImageFunction(
       this,
@@ -45,8 +60,22 @@ export class PortfolioAppStack extends cdk.Stack {
           originRequestPolicy:
             cloudfront.OriginRequestPolicy.ALL_VIEWER_EXCEPT_HOST_HEADER,
         },
+        domainNames: [props.customDomainName],
+        certificate,
       },
     );
+
+    const zone = route53.HostedZone.fromLookup(this, "PortfolioZone", {
+      domainName: props.hostedZoneDomainName,
+    });
+
+    new route53.ARecord(this, "PortfolioAliasRecord", {
+      zone,
+      recordName: props.customDomainName,
+      target: route53.RecordTarget.fromAlias(
+        new route53targets.CloudFrontTarget(distribution),
+      ),
+    });
 
     const bucket = s3.Bucket.fromBucketArn(
       this,
