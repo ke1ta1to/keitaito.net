d8ff1b891eb125465840b544b6e18d586623be18
Keita Ito <keitaito@keitaito.net>
2025-04-13 19:45:44 +0900

    feat: 各ページにメタデータを追加し、リンクカードの画像スタイルを改善

--- src/app/works/(pages)/layout.tsx
+++ src/app/works/(pages)/layout.tsx
@@ -1,3 +1,12 @@
+import type { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: {
+    default: "Works | Keita Ito",
+    template: "%s | Works | Keita Ito",
+  },
+};
+
 export default function WorksLayout({
   children,
 }: {
