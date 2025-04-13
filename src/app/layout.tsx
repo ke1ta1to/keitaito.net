486ce870a77f3202dd66d504528e7fa7a6c4d5f7
Keita Ito <keitaito@keitaito.net>
2025-04-13 14:54:01 +0900

    feat: タイトルと説明を設定

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,3 +1,4 @@
+import type { Metadata } from "next";
 import type { PropsWithChildren } from "react";
 
 import { AppLayout } from "@/components/app-layout";
@@ -5,6 +6,14 @@ import { AppLayout } from "@/components/app-layout";
 import "./globals.css";
 
 export const dynamic = "force-dynamic";
+
+export const metadata: Metadata = {
+  title: {
+    default: "Keita Ito",
+    template: "%s | Keita Ito",
+  },
+  description: "伊藤啓太のポートフォリオサイト",
+};
 
 export default function RootLayout({ children }: PropsWithChildren) {
   return (
