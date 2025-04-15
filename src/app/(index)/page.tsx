173cd089f1273a67aa11df75c4e145d4bf09998a
Keita Ito <keitaito@keitaito.net>
2025-04-15 15:43:24 +0900

    feat: schema-dtsを追加し、JSON-LDコンポーネントを実装

--- src/app/(index)/page.tsx
+++ src/app/(index)/page.tsx
@@ -1,6 +1,7 @@
 import { ActivitiesCard } from "./_components/activities-card";
 import { ArticlesCard } from "./_components/articles-card";
 import { ContactCard } from "./_components/contact-card";
+import { JsonLd } from "./_components/json-lg";
 import { ProfileCard } from "./_components/profile-card";
 import { SkillsCard } from "./_components/skills-card";
 import { WorksCard } from "./_components/works-card";
@@ -18,29 +19,32 @@ export default async function IndexPage() {
   const articles = await fetchArticles();
 
   return (
-    <div className="grid gap-4 md:grid-cols-2">
-      <div className="contents space-y-0 md:block md:space-y-4">
-        <div className="order-1 md:order-none">
-          <ProfileCard profile={profile} />
+    <>
+      <div className="grid gap-4 md:grid-cols-2">
+        <div className="contents space-y-0 md:block md:space-y-4">
+          <div className="order-1 md:order-none">
+            <ProfileCard profile={profile} />
+          </div>
+          <div className="order-2 md:order-none">
+            <ActivitiesCard activities={sortedActivities} maxActivities={4} />
+          </div>
+          <div className="order-6 md:order-none">
+            <ContactCard />
+          </div>
         </div>
-        <div className="order-2 md:order-none">
-          <ActivitiesCard activities={sortedActivities} maxActivities={4} />
-        </div>
-        <div className="order-6 md:order-none">
-          <ContactCard />
+        <div className="contents space-y-0 md:block md:space-y-4">
+          <div className="order-3 md:order-none">
+            <ArticlesCard articles={articles} maxArticles={4} />
+          </div>
+          <div className="order-4 md:order-none">
+            <WorksCard works={works} />
+          </div>
+          <div className="order-5 md:order-none">
+            <SkillsCard skills={skills} />
+          </div>
         </div>
       </div>
-      <div className="contents space-y-0 md:block md:space-y-4">
-        <div className="order-3 md:order-none">
-          <ArticlesCard articles={articles} maxArticles={4} />
-        </div>
-        <div className="order-4 md:order-none">
-          <WorksCard works={works} />
-        </div>
-        <div className="order-5 md:order-none">
-          <SkillsCard skills={skills} />
-        </div>
-      </div>
-    </div>
+      <JsonLd />
+    </>
   );
 }
