abc3f251c639e32e2a738e8292f78432ee0e41fa
Keita Ito <keitaito@keitaito.net>
2025-06-22 00:30:32 +0900

    chore: add storybook

--- apps/web/eslint.config.mjs
+++ apps/web/eslint.config.mjs
@@ -1,4 +1,6 @@
+// For more info, see https://github.com/storybookjs/eslint-plugin-storybook#configuration-flat-config-format
 import { FlatCompat } from "@eslint/eslintrc";
+import storybook from "eslint-plugin-storybook";
 
 const compat = new FlatCompat({
   baseDirectory: import.meta.dirname,
@@ -25,6 +27,7 @@ const eslintConfig = [
       ],
     },
   }),
+  ...storybook.configs["flat/recommended"],
 ];
 
 export default eslintConfig;
