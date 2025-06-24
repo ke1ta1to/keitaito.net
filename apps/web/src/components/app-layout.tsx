baa08798a34e9c55ccd54c97ff5ebaf4db8f1dde
Keita Ito <keitaito@keitaito.net>
2025-06-24 14:56:48 +0900

    fix: ヘッダーとバックグラウンドカラーの修正

--- apps/web/src/components/app-layout.tsx
+++ apps/web/src/components/app-layout.tsx
@@ -7,10 +7,10 @@ export default async function AppLayout({
   children: React.ReactNode;
 }) {
   return (
-    <>
+    <div className="from-primary/20 via-secondary/10 to-primary/20 min-h-screen bg-gradient-to-br">
       <AppHeader />
       {children}
       <AppFooter />
-    </>
+    </div>
   );
 }
