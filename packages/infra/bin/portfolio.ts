2c323d67b9e4067dc7999e4866ed6ad6b0a4248a
Keita Ito <keitaito@keitaito.net>
2025-06-28 06:32:35 +0900

    feat: 環境変数ファイルを追加し、ポートフォリオスタックにドメイン設定を統合

--- packages/infra/bin/portfolio.ts
+++ packages/infra/bin/portfolio.ts
@@ -1,6 +1,9 @@
 #!/usr/bin/env node
 import * as cdk from "aws-cdk-lib";
+import * as dotenv from "dotenv";
 import { PortfolioStack } from "../lib/portfolio-stack";
+
+dotenv.config();
 
 const app = new cdk.App();
 new PortfolioStack(app, "PortfolioStack", {
@@ -9,9 +12,17 @@ new PortfolioStack(app, "PortfolioStack", {
    * but a single synthesized template can be deployed anywhere. */
   /* Uncomment the next line to specialize this stack for the AWS Account
    * and Region that are implied by the current CLI configuration. */
-  // env: { account: process.env.CDK_DEFAULT_ACCOUNT, region: process.env.CDK_DEFAULT_REGION },
+  env: {
+    account: process.env.CDK_DEFAULT_ACCOUNT,
+    region: process.env.CDK_DEFAULT_REGION,
+  },
   /* Uncomment the next line if you know exactly what Account and Region you
    * want to deploy the stack to. */
   // env: { account: '123456789012', region: 'us-east-1' },
   /* For more information, see https://docs.aws.amazon.com/cdk/latest/guide/environments.html */
+  certificateArn: process.env.CERTIFICATE_ARN as string,
+  customDomainName: process.env.CUSTOM_DOMAIN_NAME as string,
+  hostedZoneDomainName: process.env.HOSTED_ZONE_DOMAIN_NAME as string,
+  originDomainName: process.env.ORIGIN_DOMAIN_NAME as string,
+  xOriginVerifyHeader: process.env.X_ORIGIN_VERIFY_HEADER as string,
 });
--- packages/infra/lib/portfolio-stack.ts
+++ packages/infra/lib/portfolio-stack.ts
@@ -2,13 +2,29 @@ import * as cdk from "aws-cdk-lib";
 import { Construct } from "constructs";
 import * as cloudfront from "aws-cdk-lib/aws-cloudfront";
 import * as cloudfront_origins from "aws-cdk-lib/aws-cloudfront-origins";
+import * as certificatemanager from "aws-cdk-lib/aws-certificatemanager";
+import * as route53 from "aws-cdk-lib/aws-route53";
+import * as route53_targets from "aws-cdk-lib/aws-route53-targets";
 
+interface PortfolioStackProps extends cdk.StackProps {
+  customDomainName: string;
+  hostedZoneDomainName: string;
+  certificateArn: string;
+  originDomainName: string;
+  xOriginVerifyHeader: string;
+}
 export class PortfolioStack extends cdk.Stack {
-  constructor(scope: Construct, id: string, props?: cdk.StackProps) {
+  constructor(scope: Construct, id: string, props: PortfolioStackProps) {
     super(scope, id, props);
 
-    const originDomainName = "srv1.eguchi.cc";
-    const XOriginVerifyHeader = "replace-with-your-header-value";
+    const originDomainName = props.originDomainName;
+    const XOriginVerifyHeader = props.xOriginVerifyHeader;
+
+    const certificate = certificatemanager.Certificate.fromCertificateArn(
+      this,
+      "PortfolioCertificate",
+      props.certificateArn,
+    );
 
     const distribution = new cloudfront.Distribution(
       this,
@@ -22,15 +38,31 @@ export class PortfolioStack extends cdk.Stack {
               "X-Origin-Verify": XOriginVerifyHeader,
             },
           }),
-          cachePolicy: cloudfront.CachePolicy.CACHING_OPTIMIZED,
+          cachePolicy:
+            cloudfront.CachePolicy
+              .USE_ORIGIN_CACHE_CONTROL_HEADERS_QUERY_STRINGS,
           originRequestPolicy:
             cloudfront.OriginRequestPolicy.ALL_VIEWER_EXCEPT_HOST_HEADER,
           viewerProtocolPolicy:
             cloudfront.ViewerProtocolPolicy.REDIRECT_TO_HTTPS,
           allowedMethods: cloudfront.AllowedMethods.ALLOW_ALL,
         },
+        domainNames: [props.customDomainName],
+        certificate,
       },
     );
+
+    const zone = route53.HostedZone.fromLookup(this, "PortfolioZone", {
+      domainName: props.hostedZoneDomainName,
+    });
+
+    new route53.ARecord(this, "PortfolioAliasRecord", {
+      zone,
+      recordName: props.customDomainName,
+      target: route53.RecordTarget.fromAlias(
+        new route53_targets.CloudFrontTarget(distribution),
+      ),
+    });
 
     new cdk.CfnOutput(this, "DistributionDomainName", {
       value: distribution.distributionDomainName,
