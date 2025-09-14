8985705a8b79ebdd83c13eca3d266e41750c47bb
Keita Ito <keitaito@keitaito.net>
2025-09-14 18:19:19 +0900

    feat: add swagger

--- packages/server/apps/bff/src/main.ts
+++ packages/server/apps/bff/src/main.ts
@@ -1,8 +1,17 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.setGlobalPrefix('api');
+
+  const config = new DocumentBuilder().build();
+  const documentFactory = () => SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('docs', app, documentFactory, {
+    useGlobalPrefix: true,
+  });
+
   await app.listen(process.env.port ?? 3000);
 }
 
