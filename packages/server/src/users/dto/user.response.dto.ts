e387fe45727f08fd53eee9917431a9dfb4309ce6
Keita Ito <keitaito@keitaito.net>
2025-10-10 20:08:28 +0900

    feat: passwordフィールドにセキュリティ属性を追加

--- packages/server/src/users/dto/user.response.dto.ts
+++ packages/server/src/users/dto/user.response.dto.ts
@@ -1,10 +1,13 @@
+import { ApiHideProperty } from '@nestjs/swagger';
+import { Exclude } from 'class-transformer';
+
 import type { User } from '@/generated/prisma';
 
 export class UserResponseDto implements User {
   id: number;
   name: string | null;
   email: string;
-  password: string;
+  @Exclude() @ApiHideProperty() password: string;
   createdAt: Date;
   updatedAt: Date;
 }
