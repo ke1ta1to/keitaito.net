e12cf7c143c12bdb47c00df7606c580ae9d768ce
Keita Ito <keitaito@keitaito.net>
2025-10-22 01:16:49 +0900

    feat: loggerの設定

--- server/src/prisma/prisma.service.ts
+++ server/src/prisma/prisma.service.ts
@@ -1,10 +1,37 @@
-import { Injectable, OnModuleInit } from '@nestjs/common';
+import { Injectable, Logger, OnModuleInit } from '@nestjs/common';
 
 import { PrismaClient } from '@/generated/prisma';
 
 @Injectable()
-export class PrismaService extends PrismaClient implements OnModuleInit {
+export class PrismaService
+  extends PrismaClient<{
+    log: [
+      { emit: 'event'; level: 'info' },
+      { emit: 'event'; level: 'query' },
+      { emit: 'event'; level: 'warn' },
+      { emit: 'event'; level: 'error' },
+    ];
+  }>
+  implements OnModuleInit
+{
+  private readonly logger = new Logger(PrismaService.name);
+
+  constructor() {
+    super({
+      log: [
+        { emit: 'event', level: 'info' },
+        { emit: 'event', level: 'query' },
+        { emit: 'event', level: 'warn' },
+        { emit: 'event', level: 'error' },
+      ],
+    });
+  }
+
   async onModuleInit() {
+    this.$on('info', (e) => this.logger.log(e.message));
+    this.$on('query', (e) => this.logger.log(`Query: ${e.query}`));
+    this.$on('warn', (e) => this.logger.warn(e.message));
+    this.$on('error', (e) => this.logger.error(e.message));
     await this.$connect();
   }
 }
