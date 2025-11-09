4cb65efc856b6593d8272174735fb08ab6e548ab
Keita Ito <keitaito@keitaito.net>
2025-11-09 18:38:22 +0900

    refactor: Prismaのimport方法を変更

--- server/src/prisma/prisma.filter.ts
+++ server/src/prisma/prisma.filter.ts
@@ -3,11 +3,10 @@ import { BaseExceptionFilter } from '@nestjs/core';
 import { Response } from 'express';
 
 import { Prisma } from '@/generated/prisma';
-import { PrismaClientKnownRequestError } from '@/generated/prisma/runtime/library';
 
 @Catch(Prisma.PrismaClientKnownRequestError)
 export class PrismaFilter extends BaseExceptionFilter {
-  catch(exception: PrismaClientKnownRequestError, host: ArgumentsHost) {
+  catch(exception: Prisma.PrismaClientKnownRequestError, host: ArgumentsHost) {
     const ctx = host.switchToHttp();
     const response = ctx.getResponse<Response>();
     const message = exception.message.replace(/\n/g, '');
