a4ef60133186f82364f1fbf42e1da1851a81d320
Keita Ito <keitaito@keitaito.net>
2026-01-30 03:11:00 +0900

    feat: shadcnのtheme導入

--- packages/www/src/app/layout.tsx
+++ packages/www/src/app/layout.tsx
@@ -1,9 +1,15 @@
 import { Providers } from "@/components/providers";
 import "./globals.css";
+import { JetBrains_Mono } from "next/font/google";
+
+const jetbrainsMono = JetBrains_Mono({
+  subsets: ["latin"],
+  variable: "--font-sans",
+});
 
 export default function RootLayout({ children }: LayoutProps<"/">) {
   return (
-    <html lang="ja">
+    <html lang="ja" className={jetbrainsMono.variable}>
       <body>
         <Providers>{children}</Providers>
       </body>
