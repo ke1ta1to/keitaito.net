8a928cbec98d30314438583124a4041c94223775
Keita Ito <keitaito@keitaito.net>
2026-02-19 05:08:30 +0900

    feat: install shadcn

--- apps/web/src/app/(index)/page.tsx
+++ apps/web/src/app/(index)/page.tsx
@@ -1,3 +1,10 @@
+import { Button } from "@repo/ui/components/button";
+
 export default async function IndexPage() {
-  return <p className="text-red-500">Hello</p>;
+  return (
+    <div>
+      <p className="text-red-500">Hello</p>
+      <Button>Button</Button>
+    </div>
+  );
 }
