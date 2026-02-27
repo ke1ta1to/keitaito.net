92759017d25f1a646fd1813ff735dd02593e8a80
Keita Ito <keitaito@keitaito.net>
2026-02-27 09:29:34 +0900

    fix: set shadcn theme

--- apps/blog/src/app/layout.tsx
+++ apps/blog/src/app/layout.tsx
@@ -1,10 +1,17 @@
+import { JetBrains_Mono } from "next/font/google";
+
 import "@/styles.css";
+
+const jetbrainsMono = JetBrains_Mono({
+  subsets: ["latin"],
+  variable: "--font-sans",
+});
 
 export default async function RootLayout(prpos: LayoutProps<"/">) {
   const { children } = prpos;
 
   return (
-    <html lang="ja">
+    <html lang="ja" className={jetbrainsMono.variable}>
       <body>{children}</body>
     </html>
   );
