5ce8edafee5a53e9bf28ad44a3539e3efe84b92c
Keita Ito <keitaito@keitaito.net>
2026-03-09 09:58:31 +0900

    fix: adminのconfigをssgに

--- apps/admin/next.config.ts
+++ apps/admin/next.config.ts
@@ -3,9 +3,10 @@ import * as path from "path";
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  output: "standalone",
+  output: "export",
   transpilePackages: ["@repo/ui"],
   outputFileTracingRoot: path.join(__dirname, "../../"),
+  basePath: "/admin",
 };
 
 export default nextConfig;
