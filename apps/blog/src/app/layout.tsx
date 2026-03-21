f356d5978fbc5fce133bcc8f2b5d6300963882f0
Keita Ito <keitaito@keitaito.net>
2026-03-22 06:14:10 +0900

    feat: install nextjs

---
+++ apps/blog/src/app/layout.tsx
@@ -0,0 +1,11 @@
+import "../styles.css";
+
+export default async function RootLayout(props: LayoutProps<"/">) {
+  const { children } = props;
+
+  return (
+    <html lang="ja">
+      <body>{children}</body>
+    </html>
+  );
+}
