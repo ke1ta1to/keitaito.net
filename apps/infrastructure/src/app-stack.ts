45abfb5323d5e3b7a2fc9ee59f4cfcdf2ea6fa95
Keita Ito <keitaito@keitaito.net>
2026-04-03 21:15:36 +0900

    fix: delete ec2

--- apps/infrastructure/src/app-stack.ts
+++ apps/infrastructure/src/app-stack.ts
@@ -1,5 +1,4 @@
 import * as cdk from "aws-cdk-lib";
-import * as ec2 from "aws-cdk-lib/aws-ec2";
 import * as ecr from "aws-cdk-lib/aws-ecr";
 import * as iam from "aws-cdk-lib/aws-iam";
 import type { Construct } from "constructs";
@@ -16,12 +15,11 @@ export class AppStack extends cdk.Stack {
     super(scope, id, props);
 
     this.props = props;
-    this.createEcrRepository();
-    this.createEc2();
+    this.ecrRepository = this.createEcrRepository();
   }
 
-  private createEcrRepository() {
-    this.ecrRepository = new ecr.Repository(this, "BlogImageRepository", {
+  private createEcrRepository(): ecr.Repository {
+    const repository = new ecr.Repository(this, "BlogImageRepository", {
       repositoryName: "blog",
       removalPolicy: cdk.RemovalPolicy.DESTROY,
       emptyOnDelete: true,
@@ -44,77 +42,16 @@ export class AppStack extends cdk.Stack {
         },
       }),
     });
-    this.ecrRepository.grantPullPush(githubActionsRole);
+    repository.grantPullPush(githubActionsRole);
 
     new cdk.CfnOutput(this, "RepositoryUri", {
-      value: this.ecrRepository.repositoryUri,
+      value: repository.repositoryUri,
     });
 
     new cdk.CfnOutput(this, "GitHubActionsRoleArn", {
       value: githubActionsRole.roleArn,
     });
-  }
 
-  private createEc2() {
-    const vpc = new ec2.Vpc(this, "Vpc", {
-      maxAzs: 1,
-      natGateways: 0,
-      subnetConfiguration: [
-        {
-          name: "Public",
-          subnetType: ec2.SubnetType.PUBLIC,
-          cidrMask: 24,
-        },
-      ],
-    });
-
-    const securityGroup = new ec2.SecurityGroup(this, "Ec2SecurityGroup", {
-      vpc,
-    });
-    securityGroup.addIngressRule(ec2.Peer.anyIpv4(), ec2.Port.tcp(80));
-    securityGroup.addIngressRule(ec2.Peer.anyIpv4(), ec2.Port.tcp(443));
-    securityGroup.addIngressRule(ec2.Peer.anyIpv4(), ec2.Port.tcp(22));
-
-    const instanceRole = new iam.Role(this, "Ec2InstanceRole", {
-      assumedBy: new iam.ServicePrincipal("ec2.amazonaws.com"),
-    });
-    instanceRole.addManagedPolicy(
-      iam.ManagedPolicy.fromAwsManagedPolicyName(
-        "AmazonSSMManagedInstanceCore",
-      ),
-    );
-    this.ecrRepository.grantPull(instanceRole);
-
-    const instance = new ec2.Instance(this, "Ec2Instance", {
-      vpc,
-      securityGroup,
-      role: instanceRole,
-      requireImdsv2: true,
-      machineImage: ec2.MachineImage.latestAmazonLinux2023({
-        cpuType: ec2.AmazonLinuxCpuType.ARM_64,
-      }),
-      instanceType: ec2.InstanceType.of(
-        ec2.InstanceClass.T4G,
-        ec2.InstanceSize.MICRO,
-      ),
-      keyPair: ec2.KeyPair.fromKeyPairName(this, "Ec2KeyPair", "ec2-key-pair"),
-    });
-
-    instance.addUserData(
-      "yum update -y",
-      "yum install -y docker",
-      "systemctl enable docker",
-      "systemctl start docker",
-      "usermod -aG docker ec2-user",
-    );
-
-    const eip = new ec2.CfnEIP(this, "ElasticIp", {
-      domain: "vpc",
-      instanceId: instance.instanceId,
-    });
-
-    new cdk.CfnOutput(this, "ElasticIpAddress", {
-      value: eip.ref,
-    });
+    return repository;
   }
 }
